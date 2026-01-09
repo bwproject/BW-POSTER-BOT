7a76624cd8ec70b3b6a94dc17613a5956ec00623
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2026-01-09 15:15:18 +0500

    Integrate logging into the scheduler
    Added logging to the scheduler for better monitoring.

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,11 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import logging
+
+log = logging.getLogger("SCHEDULER")
 
 scheduler = AsyncIOScheduler()
+
+
+def start_scheduler():
+    scheduler.start()
+    log.info("Планировщик запущен")
